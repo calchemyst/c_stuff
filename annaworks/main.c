ffa56cb9f4b9d38fd9c95a0fca8bda7d5975cfcb
Anna Fuller <afuller@palantir.com>
2015-07-13 12:50:50 -0700

    Initial Commit

---
+++ annaworks/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  annaworks
+//
+//  Created by Anna Fuller on 7/13/15.
+//  Copyright (c) 2015 Anna Fuller. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
