e051bf1e0d470762470e1ffc7175a705e1250801
Anna Fuller <afuller@palantir.com>
2015-07-14 16:26:38 -0700

    Some bugfixes

--- annaworks/data.c
+++ annaworks/data.c
@@ -17,16 +17,21 @@ void reverse(char to[], char from[], int len);
 void copy(char to[], char from[]);
 void squeeze(char s1[], char s2[]);
 void squeezehelper(char s[], int c);
+int any(char s1[], char s2[]);
+int anyhelper(char s[], int c);
 
 int main() {
     char s[] = "2AF3";
     char h[] = "hokkahokka";
     char h2[] = "0FF3";
+    char h3[] = "k";
     char s1[] = "hotdoghot";
     char s2[] = "dg";
     int i = htoi(s);
     int j = htoi(h);
     int k = htoi(h2);
+    int k1 = any(h, h3);
+    printf("Found at %d\n", k1);
     squeeze(s1, s2);
     printf("%s\n", s1);
     printf("Answer is %d\n", i);
@@ -67,12 +72,12 @@ int htoi(char s[]) {
     n = 0;
     for (i = 0; s[i] != '\0'; ++i) {
         n = 16*n;
-        if ((s[i] >= 'A' && s[i] <= 'F') || (s[i] >= 'a' && s[i] <= 'f')) {
-            s[i] = s[i] -7 ;
+        if ((s[i] >= 'A' & s[i] <= 'f') && !(s[i] > 'F' && s[i] < 'a')) {
+            s[i] -= 7 ;
             if (s[i] >= 'a' && s[i] <= 'f') {
-                s[i] = s[i] - 32;
+                s[i] -= 32;
             }
-        } else if (s[i] >= '9') {
+        } else if ((s[i] > '9' && s[i] < 'A')) {
             printf("Not a hexadecimal.\n");
             return 0;
         }
@@ -100,4 +105,27 @@ void squeeze(char s1[], char s2[]) {
     for (i = 0; s2[i] != '\0'; i++) {
         squeezehelper(s1, s2[i]);
     }
+}
+
+int any(char s1[], char s2[]) {
+    int i, k;
+    k = -1;
+    for (i = 0; s2[i] != '\0'; i++) {
+        k = anyhelper(s1, s2[i]);
+        if (k != -1) {
+            return k;
+        }
+    }
+    return k;
+}
+
+int anyhelper(char s[], int c) {
+    int i, k;
+    k = -1;
+    for (i = 0; s[i] != '\0'; i++) {
+        if (s[i] == c) {
+            return i;
+        }
+    }
+    return k;
 }
